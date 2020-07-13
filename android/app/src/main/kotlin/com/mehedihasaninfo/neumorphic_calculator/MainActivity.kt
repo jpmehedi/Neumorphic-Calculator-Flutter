ec987280902fa196349b41a004c5aebc644d4e27
Mehedi Hasan <mehedihasan.cse.jp@gmail.com>
2020-07-13 10:09:00 +0600

    init

---
+++ android/app/src/main/kotlin/com/mehedihasaninfo/neumorphic_calculator/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mehedihasaninfo.neumorphic_calculator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'Neumorphic Calculator',
+      home: CalculatorScreen(),
+    ),
+  );
+}
+
+class CalculatorScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
